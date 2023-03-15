34c54a3a2e7b0f28c264f4694ffb4007e7fc0545
Neelkanth7134 <118507408+Neelkanth7134@users.noreply.github.com>
2023-03-15 11:33:09 +0530

    Update Abc.java

--- nilesh/src/nilesh/Abc.java
+++ nilesh/src/nilesh/Abc.java
@@ -5,6 +5,8 @@ public class Abc {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("good morning 1");
+		
+		System.out.println("good evening");
 	}
 
 }
